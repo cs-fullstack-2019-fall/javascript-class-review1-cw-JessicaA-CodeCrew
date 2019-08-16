059a263400140b52799f789a84cfd692aa2b2323
JessicaA-CodeCrew <ms.jessica.avarie@gmail.com>
2019-08-16 12:41:59 -0500

    Review1CW

---
+++ index.js
@@ -0,0 +1,42 @@
+//Problem 1:
+// Put the start of your program in a main function. Create a class for Books with name, rating, genre, and author. Create 3 instances of Books and put them in an array. Afterwards print each one in the format below:
+//
+// The book [BOOK NAME] has [RATING] stars!!!
+
+function main()
+{
+    let myBooks = new Books("You Get a Hundred", "5", "Business","Oprah" );
+    //console.log(myBooks);
+
+    let anotherBook = new Books("Jedi Secrets", "5", "CoolIsh", "KYancy");
+    //console.log(anotherBook);
+
+    let thirdBook = new Books ("Life of the Oracle", "10", "Universal", "JAvarie");
+    //console.log(thirdBook);
+
+    let allBooks = [myBooks, anotherBook, thirdBook];
+    allBooks.forEach(function (eachElement)
+    {
+        console.log(eachElement);
+
+    });
+
+}
+
+
+class Books
+    {
+        constructor(name, rating, genre, author)
+        {
+            this.name = name;
+            this.rating = rating;
+            this.genre = genre;
+            this.author = author;
+        }
+
+    }
+
+
+
+main();
+
